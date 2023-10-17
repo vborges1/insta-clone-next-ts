a1642edb37335565ca99876113d4957fd71ecf55
Vitor Borges <vsilveira@techsocial.com.br>
2023-10-17 07:43:55 -0300

    feat: responsividade no feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,13 +3,13 @@ import Stories from "./Stories";
 
 export default function Feed() {
     return (
-        <main>
-            <section>
+        <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+            <section className="md:col-span-2">
                 <Stories />
                 <Posts />
             </section>
 
-            <section>a</section>
+            <section className="hidden md:inline-grid md:col-span-1">a</section>
         </main>
     )
 }
